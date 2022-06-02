b3774d4b36908695df93b119bd007702272dad66
guilherme2912 <guilherme2912@hotmail.com>
2022-06-01 22:20:27 -0300

    Correção do codigo, utilizando os valores padrões!

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,31 +1,7 @@
 var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0;
 
-// function increment() {
-//     currentNumber = currentNumber + 1;
-//     currentNumberWrapper.innerHTML = currentNumber;
-// }
-
-// function decrement() {
-//         currentNumber = currentNumber - 1;
-//         currentNumberWrapper.innerHTML = currentNumber;
-// }
-
-function subtrair(){
-    if(currentNumber < 0) {
-        currentNumber = currentNumber - 1;
-        currentNumberWrapper.innerHTML = currentNumber;
-       
-    } else {
-        document.getElementById('currentNumber').style.color = 'red';
-        currentNumber = currentNumber - 1;
-        currentNumberWrapper.innerHTML = currentNumber;
-        // document.getElementById('subtrair').disable = true;
-        // alert("Proibido número negativo");
-    }
-}
-
-function adicionar() {
+function increment() {
     if(currentNumber < 0){
         currentNumber = currentNumber + 1;
         currentNumberWrapper.innerHTML = currentNumber;
@@ -36,5 +12,17 @@ function adicionar() {
     }
 }
 
+function decrement() {
+    if(currentNumber <= 0) {
+        document.getElementById('currentNumber').style.color = 'red';
+        currentNumber = currentNumber - 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+    } else {
+        currentNumber = currentNumber - 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+        // document.getElementById('subtrair').disable = true;
+        // alert("Proibido número negativo");
+    }
+}
 
 
