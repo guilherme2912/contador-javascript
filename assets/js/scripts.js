8b682226ee7f9924ab64175222e843c9e6e50111
guilherme2912 <guilherme2912@hotmail.com>
2022-06-01 21:39:27 -0300

    Regra quando o número for negativo ele bloquear o botão de subtrair e aparecer um alerta.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,28 @@
 var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0;
 
-function increment() {
+// function increment() {
+//     currentNumber = currentNumber + 1;
+//     currentNumberWrapper.innerHTML = currentNumber;
+// }
+
+// function decrement() {
+//         currentNumber = currentNumber - 1;
+//         currentNumberWrapper.innerHTML = currentNumber;
+// }
+
+function subtrair(){
+    if(currentNumber <= 0) {
+        document.getElementById('subtrair').disable = true;
+        alert("Proibido número negativo");
+    } else {
+        currentNumber = currentNumber - 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+    }
+}
+
+function adicionar() {
     currentNumber = currentNumber + 1;
     currentNumberWrapper.innerHTML = currentNumber;
 }
 
-function decrement() {
-    currentNumber = currentNumber - 1;
-    currentNumberWrapper.innerHTML = currentNumber;
-}
